e10ae0a1e90c3b62156fe9a5b3c83cff31f2f2ac
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:06:06 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-bit-pack-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -22,8 +22,14 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-bit-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-bit-index-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-bit-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-bit-index-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -37,7 +43,10 @@ let package = Package(
             name: "Bit Pack Primitives Test Support",
             dependencies: [
                 "Bit Pack Primitives",
-                .product(name: "Bit Index Primitives Test Support", package: "swift-bit-index-primitives"),
+                .product(
+                    name: "Bit Index Primitives Test Support",
+                    package: "swift-bit-index-primitives"
+                ),
             ],
             path: "Tests/Support"
         ),
